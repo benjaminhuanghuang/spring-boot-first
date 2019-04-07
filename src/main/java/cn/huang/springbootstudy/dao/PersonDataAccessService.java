cc837ac1bf075b55badb9b52084713d5f601a8eb
Benjamin Huang <bhuang@rms.com>
2019-04-06 22:28:59 -0700

    update

---
+++ src/main/java/cn/huang/springbootstudy/dao/PersonDataAccessService.java
@@ -0,0 +1,35 @@
+package cn.huang.springbootstudy.dao;
+
+import cn.huang.springbootstudy.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+@Repository("postgres")
+public class PersonDataAccessService implements  PersonDao{
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return Arrays.asList(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+}
