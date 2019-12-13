18e92c0b5d3e0c67225899ef50f4c9a8f554a8a0
Benjamin Huang <bhuang@rms.com>
2019-12-12 16:08:52 -0800

    add sample

---
+++ springboot-web-app/src/main/java/ben/study/data/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package ben.study.data.repository;
+
+import ben.study.data.entity.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
